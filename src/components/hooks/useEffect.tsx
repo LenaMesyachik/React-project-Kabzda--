936d80861a65baacc2abde0f6d40b02b83258c33
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-19 20:07:16 +0300

    Create useEffect (with setTimout)

--- src/components/hooks/useEffect.tsx
+++ src/components/hooks/useEffect.tsx
@@ -5,27 +5,24 @@ export const UseEffect = () => {
     const [counter, setCounter] = useState(1)
     const [fake, setFake] = useState(1)
     console.log('Example')
+    /* useEffect(() => {
+         debugger
+         console.log('useEffect')
+         document.title = counter.toString()
+     }, [counter])
+ */
     useEffect(() => {
-        debugger
-        console.log('useEffect')
-        document.title = counter.toString()
-    }, [counter])
-
-    useEffect(() => {
-        debugger
-        console.log('useEffect2')
-        document.title = counter.toString()
-    }, [])
-
-    useEffect(() => {
-        debugger
-        console.log('useEffect3')
-        document.title = counter.toString()
-    }, )
+            setTimeout(() => {
+                debugger
+                console.log('setTimeout')
+                document.title = counter.toString()
+            }, 1000)
+        }
+        , )
 
     return <>
         hello {counter} {fake}
-        <button onClick={() => setFake(fake + 1)}>+</button>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
     </>
 }
