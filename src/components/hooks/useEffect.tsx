726883ab2bf06a33c217cf97fef747cd9e94f5c9
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-19 19:35:59 +0300

    Create useEffect (with different dependence

--- src/components/hooks/useEffect.tsx
+++ src/components/hooks/useEffect.tsx
@@ -3,12 +3,29 @@ import {useEffect, useState} from "react";
 
 export const UseEffect = () => {
     const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
     console.log('Example')
     useEffect(() => {
-    })
+        debugger
+        console.log('useEffect')
+        document.title = counter.toString()
+    }, [counter])
+
+    useEffect(() => {
+        debugger
+        console.log('useEffect2')
+        document.title = counter.toString()
+    }, [])
+
+    useEffect(() => {
+        debugger
+        console.log('useEffect3')
+        document.title = counter.toString()
+    }, )
 
     return <>
-        hello {counter}
+        hello {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>+</button>
         <button onClick={() => setCounter(counter + 1)}>+</button>
     </>
 }
